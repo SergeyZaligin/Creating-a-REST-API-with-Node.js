0e819756a9aefd37e4fa54b50404c828cbfb1d02
cobweb <codename.cobweb@gmail.com>
2018-05-13 17:10:51 +0300

    second commit

--- api/routes/products.js
+++ api/routes/products.js
@@ -7,11 +7,27 @@ const Product = require('../models/product');
 
 router.get('/', (req, res, next) => {
     Product.find()
+        .select('name price _id')
         .exec()
         .then(docs => {
             console.log(docs);
+            
             if (docs.length >= 0) {
-                res.status(200).json(docs);
+                const response = {
+                    count: docs.length,
+                    products: docs.map(doc => {
+                        return {
+                            name: doc.name,
+                            price: doc.price,
+                            _id: doc._id,
+                            request: {
+                                type: 'GET',
+                                url: 'http://localhost:8080/products/' + doc._id
+                            }
+                        } 
+                    })
+                }
+                res.status(200).json(response);
             } else {
                 res.status(404).json({
                     message: 'Docs not found'
@@ -35,9 +51,17 @@ router.post('/', (req, res, next) => {
     product.save()
         .then(result => {
             console.log(result);
-            res.status(200).json({
-                message: 'Handling POST requests to /products',
-                createdProduct: result
+            res.status(201).json({
+                message: 'Created product successfully',
+                createdProduct: {
+                    name: result.name,
+                    price: result.price,
+                    _id: result._id,
+                    request: {
+                        type: 'GET',
+                        url: 'http://localhost:8080/products/' + result._id
+                    }
+                }
             });        
         })
         .catch(error => {
@@ -52,11 +76,18 @@ router.get('/:productId', (req, res, next) => {
     const id = req.params.productId;
 
     Product.findById(id)
+        .select('name price _id')
         .exec()
         .then(doc => {
             console.log(doc);
             if (doc) {
-                res.status(200).json(doc);
+                res.status(200).json({
+                    product: doc,
+                    request: {
+                        type: 'GET',
+                        url: 'http://localhost:8080/products/'
+                    }
+                });
             } else {
                 res.status(404).json({
                     message: 'No valid entry found for provider ID'
@@ -82,7 +113,13 @@ router.patch('/:productId', (req, res, next) => {
     Product.update({_id: id}, { $set: updateOps })
         .exec()
         .then(result => {
-            res.status(200).json(result);
+            res.status(200).json({
+                message: 'Product updated',
+                request: {
+                    type: 'GET',
+                    url: 'http://localhost:8080/products/' + id
+                }
+            });
         })
         .catch(error => {
             console.log(error);
@@ -97,7 +134,17 @@ router.delete('/:productId', (req, res, next) => {
     Product.deleteOne({_id: id})
         .exec()
         .then(result => {
-            res.status(200).json(result);
+            res.status(200).json({
+                message: 'Product deleted',
+                request: {
+                    type: 'POST',
+                    url: 'http://localhost:8080/products/',
+                    body: {
+                        name: 'String',
+                        price: 'Number'
+                    }
+                }
+            });
         })
         .catch(error => {
             console.log(error);
